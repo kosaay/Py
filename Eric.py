ad2e59c68d2dd46ded09560a0797aad6e35e0bd8
kosas3771 <kosas3771@gmail.com>
2022-03-29 20:19:22 +0900

    1st commit

---
+++ Eric.py
@@ -0,0 +1,25 @@
+name = "Eric"
+age = 74
+print("Hello, %s. You are %s." %(name,age))
+
+print('\n')
+
+name = "Eric"
+print("Hello, %s." %name)
+
+print('\n')
+
+f_name = "Eric Idle"
+age = "74"
+profession = "comedian"
+affiliation = "Monty Python"
+print("Hello, %s. You are %s. You are a %s. You were a member of %s." %(f_name, age, profession, affiliation))
+
+print('\n')
+
+date_1 = '1980'
+python_inventor = 'Guido van Rossum'
+location = 'Centrum Wiskunde & Information'
+country = 'Netherland'
+date_2 = '1989'
+print("Python was conceived in the late %ss by %s at %s (CWI) in the %s as a successor to the ABC language (itself inspired by SETL), capable of exception handling and interfacing with the Amoeba operating system. Its implementation began in December %s." %(date_1, python_inventor, location, country, date_2)
