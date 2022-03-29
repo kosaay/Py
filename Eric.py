ccc7524c4ff422b61a01610aa6d43c6e7c5023b4
kosas3771 <kosas3771@gmail.com>
2022-03-29 20:47:47 +0900

    2nd commit

--- Eric.py
+++ Eric.py
@@ -17,9 +17,9 @@ print("Hello, %s. You are %s. You are a %s. You were a member of %s." %(f_name, 
 
 print('\n')
 
-date_1 = '1980'
-python_inventor = 'Guido van Rossum'
-location = 'Centrum Wiskunde & Information'
-country = 'Netherland'
-date_2 = '1989'
-print("Python was conceived in the late %ss by %s at %s (CWI) in the %s as a successor to the ABC language (itself inspired by SETL), capable of exception handling and interfacing with the Amoeba operating system. Its implementation began in December %s." %(date_1, python_inventor, location, country, date_2)
+date_1 = "1980"
+python_inventor = "Guido van Rossum"
+location = "Centrum Wiskunde & Information"
+country = "Netherland"
+date_2 = "1989"
+print("Python was conceived in the late %ss by %s at %s (CWI) in the %s as a successor to the ABC language (itself inspired by SETL), capable of exception handling and interfacing with the Amoeba operating system. Its implementation began in December %s." %(date_1, python_inventor, location, country, date_2))
